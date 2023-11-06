8a5b5e0daacbb608a7e9e2b76a50e9481f099295
clovisphere <clovis.mugaruka@gmail.com>
2023-11-06 09:58:49 +0300

    Add initial project structure for the go(lang) api

---
+++ backend/go/todo-api/go.mod
@@ -0,0 +1,5 @@
+module github.com/clovisphere/todo/backend/go/todo-api
+
+go 1.21.3
+
+require github.com/go-chi/chi/v5 v5.0.10
---
+++ backend/go/todo-api/server/server.go
@@ -0,0 +1,38 @@
+package server
+
+import (
+	"net"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type Server struct {
+	address string
+	mux     chi.Router
+	server  *http.Server
+}
+
+type Options struct {
+	Host string
+	Port int
+}
+
+func New(opts Options) *Server {
+	address := net.JoinHostPort(opts.Host, strconv.Itoa(opts.Port))
+	mux := chi.NewMux()
+	return &Server{
+		address: address,
+		//mux:     mux,
+		server: &http.Server{
+			Addr:              address,
+			Handler:           mux,
+			ReadTimeout:       5 * time.Second,
+			ReadHeaderTimeout: 5 * time.Second,
+			WriteTimeout:      5 * time.Second,
+			IdleTimeout:       5 * time.Second,
+		},
+	}
+}
